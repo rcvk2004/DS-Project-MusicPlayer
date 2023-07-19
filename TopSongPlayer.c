a9c36232d3c85877d7ec994ce32db4b56d23870e
Ravuri Chinmay Venu Kishore <135642903+rcvk2004@users.noreply.github.com>
2023-07-19 19:25:12 +0530

    Made slight changes in Musharaf's code

--- TopSongPlayer.c
+++ TopSongPlayer.c
@@ -1,35 +1,36 @@
 #include <stdio.h>
 #include <windows.h>
 #include <MMsystem.h>
+#include <stdlib.h>
 
 struct node {
 	char songName[30];
 	char songPath[40];
 	struct node *next;
 }*head = NULL;
-int nos=0;//no of songs
+int nos=0;		// variable to count no of songs
 
-void playSongs();
-void insert();
-void display();
+void playSongs();	// Function to play songs in a queue
+void insert();		// Function to insert songs in a queue
+void display();		// Function to display the songs present in the list
 
 int main() {
 	int choice;
-	printf("Welcome to top music player\n");
-	printf("  =============           =========\n");
+	printf("\3\3\3 Welcome to top music player \3\3\3\n");
+	printf("   ===========            =========\n");
 	printf("  |           |          |         |\n");
-	printf("  =====|  |====          |         |\n");
+	printf("   ====|  |===           |         |\n");
 	printf("       |  |      ======  |=========\n");
 	printf("       |  |     |      | |\n");
 	printf("       |  |     |      | |\n");
 	printf("        ==       ======  |\n");
 	printf("\nVersion: 1.0\n");
 	printf("Copyright 2023 Top | All Rights reserved\n");
-	PlaySound(TEXT(".\\welcome-note.WAV"),NULL,SND_SYNC);
+	PlaySound(TEXT(".\\Songs\\welcome-note.WAV"),NULL,SND_SYNC);
 	printf("\nOperations that this music player can perform\n");
 	printf("  1. Play Songs\n  2. Select from which song to play\n  3. Insert new song in playlist\n  4. Shuffle playlist\n  5. Delete a song from playlist\n");
 	printf("  6. Display playlist\n  7. Exit\n");
-	PlaySound(TEXT(".\\Info.WAV"),NULL,SND_SYNC);
+	PlaySound(TEXT(".\\Songs\\Info.WAV"),NULL,SND_SYNC);
 	
 	while (1) {
 		printf("Enter your choice: ");
@@ -39,16 +40,15 @@ int main() {
 				break;
 			case 2:
 				break;
-			case 3:
+			case 3: insert();
 				break;
 			case 4:
 				break;
 			case 5:
 				break;
-			case 6:
+			case 6: display();
 				break;
-			case 7:
-				exit(0);
+			case 7: exit(0);
 			default: printf("     Enter a valid choice !\n\n");
 		}
 	}
@@ -70,36 +70,39 @@ void playSongs() {
 	
 	printf("Reached end of playlist\n");
 }
+
 void insert(){
 	struct node *newnode;
-        newnode=(struct node*)malloc(sizeof(struct node));
+    	newnode=(struct node*)malloc(sizeof(struct node));
 	newnode->next=NULL;
-	printf("Enter the name of the song you want to insert:");
-        scanf("%[^\n]",newnode->songName);
-	printf("Enter the path of the song:");
-        scanf("%s",newnode->songPath);  
-        if(head==NULL){
+	printf("Enter the name of the song you want to insert: ");
+	scanf("%s",newnode->songName);
+	printf("Enter the path of the song: ");
+	scanf("%s",newnode->songPath);
+    	if(head==NULL){
 		head=newnode;
 	}
-        else{
-              struct node *ptr=head;
-              while(ptr->next!=NULL){
-                     ptr=ptr->next;
-	      }
-              ptr->next=newnode;
+    	else {
+        	struct node *ptr=head;
+        	while(ptr->next!=NULL){
+            		ptr=ptr->next;
+	    	}
+            	ptr->next=newnode;
 	}
 	nos++;
 }
-void display(){
+
+void display() {
 	struct node *ptr=head;
         int i=1;
+	printf("Here is you playlist: \n");
         while(ptr!=NULL){
-                 printf("%d.",i);
+                 printf("  %d. ",i);
                  i=i+1;
                  printf("%s\n",ptr->songName);
-                 printf(" %s",ptr->songPath):
 	         ptr=ptr->next;
 	}
+	printf("\n");
 }
           
 
